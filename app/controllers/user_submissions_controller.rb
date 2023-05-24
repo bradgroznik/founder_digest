8febe6831fe81f7abadb108265ba2ef0d447a038
bradgroznik <brad@groznikpr.com>
2023-05-24 06:29:52 -0400

    apply page

---
+++ app/controllers/user_submissions_controller.rb
@@ -0,0 +1,12 @@
+class UserSubmissionsController < ApplicationController
+    def create
+        UserSubmission.create!(user_submission_params)
+        # TODO: Redirect to a thank you page
+        redirect_to root_path, notice: "Thanks! Your application was received"
+    end
+
+    # whitelist the role radio button
+    def user_submission_params
+        params.require(:user_submission).permit(:first_name, :last_name, :email, :website, :job_role, :text, :plan_name)    
+    end
+end
