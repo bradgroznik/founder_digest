da42df41319f17c8f3f14593c6a9ca74631d4f46
bradgroznik <brad@groznikpr.com>
2023-06-29 06:48:17 -0400

    Dashboard and database seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Running Seeds..."
+user_count = User.count
+
+def websites
+    %w(google.com uber.com ryanckulp.com forkequity.com psu.edu allbirds.com)
+end     
+
+
+def create_user_and_project!
+    user = User.create!(email: Faker::Internet.email, password: 'password')
+    user.projects.create!(title: "#{Faker::Company.name} newsletter", description: Faker::Company.catch_phrase, website: websites.sample)
+end
+
+5.times do 
+    create_user_and_project!
+end
+
+new_user_count = User.count - user_count
+puts "Created #{new_user_count} users."
